93c10a7bfb8730d27b9cce127aab7ff89e87da03
Alexander Kluge <46349588+alexk201@users.noreply.github.com>
2025-04-04 17:50:53 +0200

    Create install.sh

---
+++ features/cuda/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# configure bash defaults: exit on any failure
+set -e
+
+# check permissions
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# source os-relase information
+. /etc/os-release
+
+ADJUSTED_ARCH="$(arch | sed 's/aarch64/arm64/g')"
+ADJUSTED_VERSION_ID="$(echo -n $VERSION_ID | tr -d ".")"
+
+wget -q https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ADJUSTED_VERSION_ID}/${ADJUSTED_ARCH}/3bf863cc.pub -O /etc/apt/keyrings/nvidia.asc
+echo "deb [signed-by=/etc/apt/keyrings/nvidia.asc] https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${ADJUSTED_VERSION_ID}/${ADJUSTED_ARCH} /" > /etc/apt/sources.list.d/nvidia.list
+apt update && apt install -y --no-install-recommends \
+    cuda-12-8 \
+    cuda-libraries-dev-12-8 \
+    libnvinfer-dev \
+    libnvinfer-plugin-dev \
+    libnvonnxparsers-dev
