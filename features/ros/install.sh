d527ba96a3a981d2c55ae17f37b956d984c3dd51
Alexander Kluge <46349588+alexk201@users.noreply.github.com>
2025-04-04 17:33:37 +0200

    Initial commit

---
+++ features/ros/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# configure bash defaults: exit on any failure
+set -e
+
+# check permissions
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# add ros repository
+wget -q https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -O /etc/apt/keyrings/ros.asc
+echo "deb [signed-by=/etc/apt/keyrings/ros.asc] https://ftp.osuosl.org/pub/ros2 noble main" > /etc/apt/sources.list.d/ros.list
+
+# install required packages
+apt update && apt install -y --no-install-recommends \
+    ros-jazzy-desktop \
+    ros-jazzy-topic-tools \
+    ros-jazzy-foxglove-bridge \
+    ros-jazzy-diagnostic-updater \
+    python3-colcon-ros \
+    python3-colcon-common-extensions
+
+# configure exports for interactive terminal
+echo "export ROS_INSTALL_PATH=/opt/ros/jazzy" >> /etc/bash.bashrc
+echo "source /opt/ros/jazzy/setup.bash" >> /etc/bash.bashrc
